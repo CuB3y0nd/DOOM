d4788945e62d158e17098e20beb82fe0a021fe8c
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2025-09-18 11:40:18 +0800

    fix: update nvim configurations

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,7 +6,15 @@ return {
       transparent_background = true,
     },
   },
-
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,65 +1,5 @@
 return {
   {
-    "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      local logo = [[
-=================     ===============     ===============   ========  ========
-\\ . . . . . . .\\   //. . . . . . .\\   //. . . . . . .\\  \\. . .\\// . . //
-||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\/ . . .||
-|| . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||
-||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||
-|| . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\ . . . . ||
-||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\_ . .|. .||
-|| . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\ `-_/| . ||
-||_-' ||  .|/    || ||    \|.  || `-_|| ||_-' ||  .|/    || ||   | \  / |-_.||
-||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \  / |  `||
-||    `'         || ||         `'    || ||    `'         || ||   | \  / |   ||
-||            .===' `===.         .==='.`===.         .===' /==. |  \/  |   ||
-||         .=='   \_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \/  |   ||
-||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \/  |   ||
-||   .=='    _-'          '-__\._-'         '-_./__-'         `' |. /|  |   ||
-||.=='    _-'                                                     `' |  /==.||
-=='    _-'                       [ @CuB3y0nd ]                        \/   `==
-\   _-'                                                                `-_   /
-  `''                                                                      ``'
-  ]]
-      opts.config.header = vim.split(logo, "\n")
-      opts.config.center = {
-        {
-          action = "ene | startinsert",
-          desc = " New File",
-          icon = " ",
-          key = "n",
-        },
-        {
-          action = "LazyExtras",
-          desc = " Lazy Extras",
-          icon = " ",
-          key = "x",
-        },
-        {
-          action = function()
-            vim.api.nvim_input("<cmd>qa<cr>")
-          end,
-          desc = " Quit",
-          icon = " ",
-          key = "q",
-        },
-      }
-    end,
-  },
-
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      opts.options = {
-        component_separators = { left = "|", right = "" },
-        section_separators = { left = " ", right = "" },
-      }
-    end,
-  },
-
-  {
     "snacks.nvim",
     opts = {
       indent = {
@@ -71,5 +11,59 @@ return {
         },
       },
     },
+  },
+
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
+          header = [[
+   =================     ===============     ===============   ========  ========
+   \\ . . . . . . .\\   //. . . . . . .\\   //. . . . . . .\\  \\. . .\\// . . //
+   ||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\/ . . .||
+   || . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||
+   ||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||
+   || . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\ . . . . ||
+   ||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\_ . .|. .||
+   || . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\ `-_/| . ||
+   ||_-' ||  .|/    || ||    \|.  || `-_|| ||_-' ||  .|/    || ||   | \  / |-_.||
+   ||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \  / |  `||
+   ||    `'         || ||         `'    || ||    `'         || ||   | \  / |   ||
+   ||            .===' `===.         .==='.`===.         .===' /==. |  \/  |   ||
+   ||         .=='   \_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \/  |   ||
+   ||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \/  |   ||
+   ||   .=='    _-'          '-__\._-'         '-_./__-'         `' |. /|  |   ||
+   ||.=='    _-'                                                     `' |  /==.||
+   =='    _-'                       [ @CuB3y0nd ]                        \/   `==
+   \   _-'                                                                `-_   /
+  `''                                                                      ``'
+ ]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        },
+      },
+    },
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.options = {
+        component_separators = { left = "|", right = "" },
+        section_separators = { left = " ", right = "" },
+      }
+    end,
   },
 }
